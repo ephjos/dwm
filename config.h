5e4508dab1de9cee027cfe63c97686d2ad5c8e27
Joseph Hines <jthines3@gmail.com>
2020-09-01 16:14:54 -0400

    Switched webcam bind to use proper encoding for my webcam

--- config.h
+++ config.h
@@ -208,7 +208,7 @@ static Key keys[] = {
 	//{ MODKEY,			XK_F8,		spawn,		SHCMD("mailsync") },
 	{ MODKEY,			XK_F9,		spawn,		SHCMD("rofi_mount") },
 	{ MODKEY,			XK_F10,		spawn,		SHCMD("rofi_unmount") },
-	{ MODKEY,			XK_F11,		spawn,		SHCMD("mpv --no-cache --no-osc --no-input-default-bindings --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)") },
+	{ MODKEY,			XK_F11,		spawn,		SHCMD("mpv --demuxer-lavf-format=video4linux2 --demuxer-lavf-o-set=input_format=mjpeg av://v4l2:/dev/video0") },
 	{ MODKEY,			XK_F12,		spawn,		SHCMD("notify") },
 	{ MODKEY,			XK_space,	zoom,		{0} },
 	{ MODKEY|ShiftMask,		XK_space,	togglefloating,	{0} },
